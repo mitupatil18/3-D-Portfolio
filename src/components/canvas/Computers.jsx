5c9c6cc090d02e62f688f98c430724739a842a62
Mitali <mitupatil39@gmail.com>
2024-01-31 06:58:31 +0530

    Your commit message

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -49,11 +49,7 @@ const ComputersCanvas = () => {
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
+        
         <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
